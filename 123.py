ce431a9d459c92a7a49dae4463adda2ec87df8de
jvaaaa <54944563+jvaaaa@users.noreply.github.com>
2019-09-29 11:23:16 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -22,6 +22,8 @@ def unlock(L, t=3):
 			a1.append(c)
 	a2 = ('').join(a1)
 	return a2
+
+
 def lock(L, t=3):
 	a3 = []
 	n=t%26
@@ -45,16 +47,39 @@ def lock(L, t=3):
 			a3.append(c)
 	a4 = ('').join(a3)
 	return a4
-print('凯撒解密输入1，凯撒加密输入2:')
-x=int(input())
-print(x)
-if x==1:
-	print('凯撒解密中')
-	L=input('请输入暗文：')
-	n=int(input('请输入位移数：'))
-	print(unlock(L,n))
-elif x==2:
-	print('凯撒加密中')
-	L=input('请输入明文：')
-	n=int(input('请输入位移数：'))
-	print(lock(L,n))
+
+
+print('输入文件输入1，手动输入内容输入2:')
+y=int(input())
+
+
+if y==1:
+	address=input('请输入文件地址:')
+	with open(address,'r') as f:
+		L=(f.read())
+		print('文件内容:',L)
+	print('凯撒解密输入1，凯撒加密输入2:')
+	x=int(input())
+	if x==1:
+		print('凯撒解密中')
+		n=int(input('请输入位移数：'))
+		print('明文:',unlock(L,n))
+	elif x==2:
+		print('凯撒加密中')
+		n=int(input('请输入位移数：'))
+		print('暗文:',lock(L,n))
+
+
+elif y==2:
+	print('凯撒解密输入1，凯撒加密输入2:')
+	x=int(input())
+	if x==1:
+		print('凯撒解密中')
+		L=input('请输入暗文：')
+		n=int(input('请输入位移数：'))
+		print('明文:',unlock(L,n))
+	elif x==2:
+		print('凯撒加密中')
+		L=input('请输入明文：')
+		n=int(input('请输入位移数：'))
+		print('暗文:',lock(L,n))
